6e17e7e272c87ae1efa7cdfc24ed536541df8a3e
Julia Rees <97849635+vibrantleaf@users.noreply.github.com>
2026-04-19 01:01:19 +0800

    dropped: fex-emu and added: box64+box86

---
+++ files/scripts/arm64/box64.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+set -o nounset
+set -o pipefail
+set -o xtrace
+set -o errexit
+mkdir -p /tmp/cloned
+
+dnf -y --refresh install binutils coreutils curl tar zstd
+
+git clone https://github.com/ptitSeb/box64.git /tmp/cloned/org.box86.box64
+mkdir -p /tmp/cloned/org.box86.box64/build
+
+(cd /tmp/cloned/org.box86.box64/build && exec \
+  cmake  -D ARM_DYNAREC=ON \
+  -D CMAKE_BUILD_TYPE=RelWithDebInfo \
+  -D BOX32=ON \
+  -D BOX32_BINFMT=ON \
+  /tmp.cloned/org.box86.box64 \
+)
+
+(cd /tmp/cloned/org.box86.box64/build && exec \
+  make \
+)
+
+(cd /tmp/cloned/org.box86.box64/build && exec \
+  make install \
+)
+
+(cd /tmp/cloned/org.box86.box64 && exec \
+  box64-bundle-x86-libs.sh \
+)
+
+(cd /tmp/cloned/org.box86.box64 && exec \
+  tar --extract --no-same-owner --file box64-bundle-x86-libs.tar.gz --directory / \
+)
+
