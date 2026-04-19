f46ba350fcbe2b3cdbe6ea745a0d06630dea77eb
Julia Rees <97849635+vibrantleaf@users.noreply.github.com>
2026-04-19 11:02:36 +0800

    fix build

--- files/scripts/arm64/box64.sh
+++ files/scripts/arm64/box64.sh
@@ -5,11 +5,14 @@ set -o xtrace
 set -o errexit
 mkdir -p /tmp/cloned
 
+# box64 build deps
 dnf -y --refresh install binutils coreutils curl tar zstd cmake make gcc g++
 
+# cloone the box64 sorce code
 git clone https://github.com/ptitSeb/box64.git /tmp/cloned/org.box86.box64
 mkdir -p /tmp/cloned/org.box86.box64/build
 
+# build box64 with box86 support
 (cd /tmp/cloned/org.box86.box64/build && exec \
   cmake  -D ARM_DYNAREC=ON \
   -D CMAKE_BUILD_TYPE=RelWithDebInfo \
@@ -18,19 +21,30 @@ mkdir -p /tmp/cloned/org.box86.box64/build
   /tmp/cloned/org.box86.box64 \
 )
 
+# build box64
 (cd /tmp/cloned/org.box86.box64/build && exec \
   make -j4 \
 )
 
+# install box64
 (cd /tmp/cloned/org.box86.box64/build && exec \
   make install \
 )
 
+# download box64's x86 and x86_64 compat libs
 (cd /tmp/cloned/org.box86.box64 && exec \
-  box64-bundle-x86-libs.sh \
+  /tmp/cloned/org.box86.box64/box64-bundle-x86-libs.sh
+  #curl -s https://api.github.com/repos/ptitSeb/box64/releases/latest \
+  #  | grep "browser_download_url.*box64-bundle-x86-libs*" \
+  #  | cut -d : -f 2,3 \
+  #  | tr -d \" \
+  #  | wget -qi -
 )
 
+# install box64's x86 and x86_64 ompat libs
 (cd /tmp/cloned/org.box86.box64 && exec \
   tar --extract --no-same-owner --file box64-bundle-x86-libs.tar.gz --directory / \
 )
 
+# clean up
+rm -f /tmp/cloned
