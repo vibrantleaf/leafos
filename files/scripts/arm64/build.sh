bd7220c334534752e59809e20968e0b3213c1fe8
Julia Rees <97849635+vibrantleaf@users.noreply.github.com>
2026-06-21 16:46:29 +0800

    fix build by dropping cloudfare warp, split `build.sh` to `amd64/build.sh` & `arm64/build.sh`,  added zen browser installed by default via flatpak.

---
+++ files/scripts/arm64/build.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -o nounset
+set -o pipefail
+set -o xtrace
+set -o errexit
+
+mkdir -p /tmp/cloned
+
+git clone https://gitlab.com/vibrantleaf/systemd-curfew.git /tmp/cloned/com.gitlab.vibrantleaf.systemd-curfew
+cp -v /tmp/cloned/com.gitlab.vibrantleaf.systemd-curfew/curfew-midnight.service /usr/lib/systemd/system/curfew-midnight.service
+
+git clone https://gist.github.com/vibrantleaf/ef3cd8a31ead32063a745fcc57b8a0de.git /tmp/cloned/com.github.gist.vibrantleaf.ef3cd8a31ead32063a745fcc57b8a0de/
+cp -v /tmp/cloned/com.github.gist.vibrantleaf.ef3cd8a31ead32063a745fcc57b8a0de/linux-set_random_windows_style_hostname_via_hostnamectl.sh /usr/bin/set_win10_style_hostname
+chmod +x /usr/bin/set_win10_style_hostname
+
+rm -rf /tmp/cloned
+
+#dnf repo list --all
