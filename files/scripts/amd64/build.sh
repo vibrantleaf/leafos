f1a392525a8a6cac06034faa18bd81a0641db040
Julia Rees <97849635+vibrantleaf@users.noreply.github.com>
2026-07-25 22:24:53 +0800

    202607252224

--- files/scripts/amd64/build.sh
+++ files/scripts/amd64/build.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+
+# boilerplate
 set -o nounset
 set -o pipefail
 set -o xtrace
@@ -6,26 +8,20 @@ set -o errexit
 
 mkdir -p /tmp/cloned
 
-git clone https://gitlab.com/vibrantleaf/systemd-curfew.git /tmp/cloned/com.gitlab.vibrantleaf.systemd-curfew
-cp -v /tmp/cloned/com.gitlab.vibrantleaf.systemd-curfew/curfew-midnight.service /usr/lib/systemd/system/curfew-midnight.service
-
+# get set_win10_style_hostname
 git clone https://gist.github.com/vibrantleaf/ef3cd8a31ead32063a745fcc57b8a0de.git /tmp/cloned/com.github.gist.vibrantleaf.ef3cd8a31ead32063a745fcc57b8a0de/
 cp -v /tmp/cloned/com.github.gist.vibrantleaf.ef3cd8a31ead32063a745fcc57b8a0de/linux-set_random_windows_style_hostname_via_hostnamectl.sh /usr/bin/set_win10_style_hostname
 chmod +x /usr/bin/set_win10_style_hostname
 
+# clean up /tmp/cloned
 rm -rf /tmp/cloned
 
 # link htop to btop
 ln -s /usr/bin/btop /usr/bin/htop
 
-#
-rm -rfv /usr/share/backgrounds
-rm -rfv /usr/share/gnome-background-properties
-mkdir -p /usr/share/backgrounds
-mkdir -p /usr/share/gnome-background-properties
-
-#dnf repo list --all
+# cleanup backgrounds dir
+rm -rfv /usr/share/backgrounds/*
+rm -rfv /usr/share/gnome-background-properties/*
 
 # remove default gnome extensions
-dnf remove -y --setopt=clean_requirements_on_remove=True gnome-shell-extension-*
-dnf install -y gnome-menus
+rm -rfv '/usr/share/gnome-shell/extensions/*'
