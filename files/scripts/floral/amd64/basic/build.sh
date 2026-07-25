b6e0d423a140cb61c42203fa449f5f98e6607279
Julia Rees <97849635+vibrantleaf@users.noreply.github.com>
2026-07-26 03:29:31 +0800

    Create build.sh
    Signed-off-by: Julia Rees <97849635+vibrantleaf@users.noreply.github.com>

---
+++ files/scripts/floral/amd64/basic/build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+
+# boilerplate
+set -o nounset
+set -o pipefail
+set -o xtrace
+set -o errexit
+
+#mkdir -p /tmp/cloned
+
+echo "Hello World, From inside the floral amd64 basic build.sh file. :)"
+
+# clean up /tmp/cloned
+#rm -rf /tmp/cloned
